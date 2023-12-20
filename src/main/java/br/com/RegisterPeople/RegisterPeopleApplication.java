cc4be43ee8c9598326fa0cf625d80b9084903e5b
felipe.henriques.ctr <felipe.henriques.ctr@i-systems.com>
2023-12-20 16:52:20 -0300

    Starting project

---
+++ src/main/java/br/com/RegisterPeople/RegisterPeopleApplication.java
@@ -0,0 +1,13 @@
+package br.com.RegisterPeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterPeopleApplication.class, args);
+	}
+
+}
